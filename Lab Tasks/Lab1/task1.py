92d422f5c78aca494d85b862e97493975e425a9a
layyana-junaid <142867946+layyana-junaid@users.noreply.github.com>
2024-08-21 09:49:43 +0500

    Create task1.py

---
+++ Lab Tasks/Lab1/task1.py
@@ -0,0 +1,9 @@
+#task1
+#creating a bmi system
+
+weight = float(input("Enter your weight: "))
+height = float(input("Enter your height: "))
+
+BMI = weight / (height*height)
+
+print("Your BMI is: ", BMI)
